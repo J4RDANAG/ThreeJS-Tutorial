a66e1c1d62c848bc0ef25ac63500baa7d6f73bd0
J4rdanAG <Jordanarchiegrahn@gmail.com>
2022-12-06 17:25:19 -0800

    add ground uv mapping

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -4,6 +4,7 @@ import {
     PerspectiveCamera,
 } from '@react-three/drei';
 import { Suspense } from 'react';
+import { Ground } from './Ground';
 import { Track } from './Track';
 
 export function Scene() {
@@ -14,10 +15,10 @@ export function Scene() {
             background={'both'}
             />
             <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
-            // camera 'angle'
-            <OrbitControls target={[-2.64, -0.71, 0.03]} />
-            //allows camera to be moved around
+            <OrbitControls target={[-2.64, -0.71, 0.03]} /> 
+            
             <Track />
+            <Ground/>
         </Suspense>
     )
 }
