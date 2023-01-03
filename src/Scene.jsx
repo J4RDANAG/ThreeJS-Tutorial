6184a37b80be3fec5c00d6a006fed95fb5ba84c7
J4rdanAG <Jordanarchiegrahn@gmail.com>
2023-01-02 21:42:41 -0800

    add final third person support

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -3,24 +3,40 @@ import {
     OrbitControls,
     PerspectiveCamera,
 } from '@react-three/drei';
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { Car } from './Car';
 import { Ground } from './Ground';
 import { Track } from './Track';
 
 export function Scene() {
+    const [thirdPerson, setThirdPerson] = useState(false);
+    const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
+
+    useEffect(() => {
+        function keydownHandler(e) {
+            if (e.key === 'k'){
+                if(thirdPerson) setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
+                setThirdPerson(!thirdPerson)
+            }
+        }
+        window.addEventListener('keydown', keydownHandler)
+        return () => window.removeEventListener('keydown', keydownHandler)
+    }, [thirdPerson])
     return(
         <Suspense fallback={null}>
             <Environment 
             files={process.env.PUBLIC_URL + '/textures/envmap.hdr'}
             background={'both'}
             />
-            <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
-            <OrbitControls target={[-2.64, -0.71, 0.03]} /> 
+            <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
+            {!thirdPerson && (
+                <OrbitControls target={[-2.64, -0.71, 0.03]} /> 
+            )}
+            
             
             <Track />
             <Ground/>
-            <Car />
+            <Car thirdPerson={thirdPerson}/>
         </Suspense>
     )
 }
